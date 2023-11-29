5f8c2ab2f8e6647453730e27738eb048e922544a
Zhenzhe Wu <yiwu5927@uni.sydney.edu.au>
2023-11-29 15:06:54 +1100

    react current time

--- new/views.py
+++ new/views.py
@@ -2,3 +2,15 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+
+# def my_view(request):
+#     if request.method == 'POST':
+#         # Process the POST data
+#         post_data = request.POST
+#         # Do something with post_data
+#         return render(request, 'my_template.html', {'post_data': post_data})
+
+
+
+
